c42d273f1d6c96bc00bcd804d82400e668080a2d
gaopeng527 <1259728004@qq.com>
2016-12-10 10:22:15 +0800

    Add files via upload

---
+++ doc.go
@@ -0,0 +1,6 @@
+// MySQL project doc.go
+
+/*
+MySQL document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,75 @@
+// MySQL project main.go
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/Go-SQL-Driver/MySQL"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/test?charset=utf8")
+	checkErr(err)
+
+	// 插入数据
+	stmt, err := db.Prepare("insert userinfo set username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("张三", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+
+	// 更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("李四", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	// 查询数据
+	rows, err := db.Query("select * from userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	// 删除数据
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
